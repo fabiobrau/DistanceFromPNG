23b85d1899759712589afdf58144f8ef48012a5d
Fabio Brau <fabio@asterixII>
2020-05-04 00:49:57 +0200

    DistanceFromPNG.py: Add main file

---
+++ DistanceFromPNG.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import argparse
+import cv2
+
+def main_process():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pics',
+                        type=str,
+                        nargs='+',
+                        metavar='Pic1 Pic2',
+                        help='''List of Pictures'''
+                       )
+    parser.add_argument('-f', '--focal-length',
+                        type = float,
+                        nargs='+',
+                        dest = 'focals',
+                        help='''List of relative focal lentgth for each picture'''
+                       )
+    parser.add_argument('-d', '--distance',
+                        type = float,
+                        nargs='+',
+                        dest = 'distances',
+                        help='''List of relative distance from each picture and
+                        the first one.'''
+                       )
+if __name__=='__main__':
+    main_process()
